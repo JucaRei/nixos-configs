8e9e11cf37689512505e8c4a048f7e161201c609
Reinaldo <reinaldo800@gmail.com>
2023-06-30 00:11:12 -0300

    fix

--- home-manager/_mixins/console/bash.nix
+++ home-manager/_mixins/console/bash.nix
@@ -41,13 +41,6 @@
                 color_prompt=
               fi
             fi
-            
-            if [ "$color_prompt" = yes ]; then
-              PS1="${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$ "
-            else
-              PS1="${debian_chroot:+($debian_chroot)}\u@\h:\w\$ ""
-            fi
-            unset color_prompt force_color_prompt
 
             if [ -f ~/.bash_aliases ]; then
               . ~/.bash_aliases
