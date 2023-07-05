56b631b91c89e118961341128e5b3c95e0085a03
Reinaldo <reinaldo800@gmail.com>
2023-07-04 22:38:47 -0300

    more stuff

--- home-manager/_mixins/console/default.nix
+++ home-manager/_mixins/console/default.nix
@@ -7,6 +7,8 @@
     ./neofetch.nix
     ./broot.nix
     ./xdg.nix
+    ./skim.nix
+    ./glow.nix
   ];
 
   home = {
@@ -125,17 +127,7 @@
       viAlias = true;
       vimAlias = true;
     };
-    skim = rec {
-      enable = true;
-      defaultCommand = "fd --type f --hidden --exclude '.git'";
-      defaultOptions = [ "--height 40%" "--inline-info" ];
-      changeDirWidgetCommand = "fd --type d --hidden --exclude '.git'";
-      fileWidgetCommand = defaultCommand;
-      fileWidgetOptions = [
-        "--preview 'bat --color always {} 2> /dev/null | head -200; highlight -O ansi -l {} ^ /dev/null | head -200 || cat {} ^ /dev/null | head -200'"
-      ];
-      historyWidgetOptions = [ "--tac" ];
-    };
+
     bottom = {
       enable = true;
       settings = {
