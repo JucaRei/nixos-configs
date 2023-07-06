d2e148f9a288f892491da3920c7bba4b92e94943
Reinaldo <reinaldo800@gmail.com>
2023-07-06 17:04:08 -0300

    fix

--- nixos/_mixins/base/default.nix
+++ nixos/_mixins/base/default.nix
@@ -53,21 +53,26 @@
 
   environment = {
     # Eject nano and perl from the system
-    defaultPackages = with pkgs; lib.mkForce [ 
-      gitMinimal home-manager micro rsync 
+    defaultPackages = with pkgs; lib.mkForce [
+      gitMinimal
+      home-manager
+      micro
+      rsync
     ];
-    systemPackages = with pkgs; [ 
-      man-pages 
-      pciutils 
-      psmisc 
-      unzip 
-      usbutils 
+
+    systemPackages = with pkgs; [
+      man-pages
+      pciutils
+      psmisc
+      unzip
+      usbutils
     ];
-  };
-  variables = {
-    EDITOR = "micro";
-    SYSTEMD_EDITOR = "micro";
-    VISUAL = "micro";
+
+    variables = {
+      EDITOR = "micro";
+      SYSTEMD_EDITOR = "micro";
+      VISUAL = "micro";
+    };
   };
 
   fonts = {
