9d8746cdf23db31225a98a6c643d9e5353b30fac
Reinaldo <reinaldo800@gmail.com>
2023-07-07 19:49:44 -0300

    fix

--- home-manager/_mixins/console/bash.nix
+++ home-manager/_mixins/console/bash.nix
@@ -44,17 +44,6 @@
             if [ -e ${config.home.homeDirectory}/.nix-profile/etc/profile.d/nix.sh ]; then
               source ${config.home.homeDirectory}/.nix-profile/etc/profile.d/nix.sh
             fi
-            
-            if [ -n "$force_color_prompt" ]; then
-              if [ -x /usr/bin/tput ] && tput setaf 1 >&/dev/null; then
-                # We have color support; assume it's compliant with Ecma-48
-                # (ISO/IEC-6429). (Lack of such support is extremely rare, and such
-                # a case would tend to support setf rather than setaf.)
-                color_prompt=yes
-              else
-                color_prompt=
-              fi
-            fi
 
             if [ -f ~/.bash_aliases ]; then
               . ~/.bash_aliases
