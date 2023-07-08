b153b146019d8f22ea9899cefb51f7860e0e8f73
Reinaldo <reinaldo800@gmail.com>
2023-07-08 13:01:20 -0300

    fix

--- nixos/_mixins/virt/virt-manager.nix
+++ nixos/_mixins/virt/virt-manager.nix
@@ -1,8 +1,21 @@
-{ pkgs, ... }: {
+{ pkgs, config, username, ... }: {
+
+  boot.extraModprobeConfig = ''
+    options kvm_intel nested=1
+    options kvm_intel emulate_invalid_guest_state=0
+    options kvm ignore_nsrs=1
+  '';                                         # Needed to run OSX-KVM 
+
+  users.groups.libvirtd.members = [ "root" "${username}" ];
+
   environment.systemPackages = with pkgs; [
     virt-manager
     spice-gtk
     swtpm
+    virt-viewer
+    qemu
+    OVMF
+    gvfs 
   ];
   security.polkit.enable = true;
   virtualisation = {
