5a3fad3f454e6d56313470676bef6bb765240649
Reinaldo <reinaldo800@gmail.com>
2023-07-10 19:18:22 -0300

    fix

--- home-manager/_mixins/console/default.nix
+++ home-manager/_mixins/console/default.nix
@@ -168,7 +168,26 @@
       enableAliases = true;
       icons = true;
     };
-    gpg.enable = false; # disabled for now
+    gnupg = {
+      enable = false; # disabled for now
+      gnupg.package = pkgs.gnupg;
+      dirmngr.enable = false;
+      agent = {
+        enable = false;
+        # allows web browsers to access the gpg-agent daemon
+        #enableBrowserSocket = false;
+        # NOTE: "gnome3" flavor only works with Xorg
+        # To reload config: gpg-connect-agent reloadagent /bye
+        #pinentryFlavor = "gtk2"; # use "tty" for console only
+        pinentryFlavor = "cursers";
+        # cache SSH keys added by the ssh-add
+        enableSSHSupport = false;
+        # set up a Unix domain socket forwarding from a remote system
+        # enables to use gpg on the remote system without exposing the private keys to the remote system
+        enableExtraSocket = false;
+        enableBrowserSocket = false;
+      };
+    };
     home-manager.enable = true;
     info.enable = true;
     jq.enable = true;
@@ -200,20 +219,6 @@
   };
 
   services = {
-    gpg-agent = {
-      enable = false;
-      # cache SSH keys added by the ssh-add
-      enableSSHSupport = true;
-      # set up a Unix domain socket forwarding from a remote system
-      # enables to use gpg on the remote system without exposing the private keys to the remote system
-      enableExtraSocket = false;
-      # allows web browsers to access the gpg-agent daemon
-      enableBrowserSocket = false;
-      # NOTE: "gnome3" flavor only works with Xorg
-      # To reload config: gpg-connect-agent reloadagent /bye
-      #pinentryFlavor = "gtk2"; # use "tty" for console only
-      pinentryFlavor = "curses"; # use "tty" for console only
-    };
     kbfs = {
       enable = true;
       #enable = false;
