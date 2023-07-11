d0424c5f1fcb5ab685abe534705a3461b0d73f4f
Reinaldo <reinaldo800@gmail.com>
2023-07-11 20:18:48 -0300

    fix

--- nixos/nitro/default.nix
+++ nixos/nitro/default.nix
@@ -15,7 +15,6 @@
     ../_mixins/virt
   ];
   boot = {
-    blacklistedKernelModules = lib.mkDefault [ "nouveau" ];
     extraModulePackages = with config.boot.kernelPackages; [ v4l2loopback ];
     extraModprobeConfig = lib.mkDefault ''
       options v4l2loopback devices=1 video_nr=13 card_label="OBS Virtual Camera" exclusive_caps=1
