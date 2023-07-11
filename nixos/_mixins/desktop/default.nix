fc74954f5b39c78846a69185a897dc263e9e0104
Reinaldo <reinaldo800@gmail.com>
2023-07-11 14:52:45 -0300

    testing

--- nixos/_mixins/desktop/default.nix
+++ nixos/_mixins/desktop/default.nix
@@ -1,11 +1,10 @@
-{ desktop, pkgs, username, lib, hostname, ... }: {
+{ desktop, pkgs, username, lib, ... }: {
   imports = [
     ../services/cups.nix
     ../hardware/gfx-intel.nix
     ../services/sane.nix
     ../services/dynamic-timezone.nix
-  ] ++ lib.optional (builtins.pathExists (./. + "/${desktop}.nix"))
-    ./${desktop}.nix;
+  ] ++ lib.optional (builtins.pathExists (./. + "/${desktop}.nix")) ("./${desktop}.nix");
 
   boot = {
     kernelParams = [ "quiet" "vt.global_cursor_default=0" "mitigations=off" ];
