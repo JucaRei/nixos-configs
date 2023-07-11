32c478643f379ce20e7f1f1b4bee88785787cfb8
Reinaldo <reinaldo800@gmail.com>
2023-07-11 14:55:35 -0300

    test typo

--- nixos/_mixins/desktop/default.nix
+++ nixos/_mixins/desktop/default.nix
@@ -4,7 +4,7 @@
     ../hardware/gfx-intel.nix
     ../services/sane.nix
     ../services/dynamic-timezone.nix
-  ] ++ lib.optional (builtins.pathExists (./. + "/${desktop}.nix")) ("./${desktop}.nix");
+  ] ++ lib.optional (builtins.pathExists (./. + /${desktop}.nix)) ./${desktop}.nix;
 
   boot = {
     kernelParams = [ "quiet" "vt.global_cursor_default=0" "mitigations=off" ];
