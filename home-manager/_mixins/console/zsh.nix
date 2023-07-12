7e37d9d1eedef779fbeb3d8ae01bacb7cf321958
Reinaldo <reinaldo800@gmail.com>
2023-07-12 14:53:59 -0300

    fix

--- home-manager/_mixins/console/zsh.nix
+++ home-manager/_mixins/console/zsh.nix
@@ -7,7 +7,8 @@
     packages = with pkgs; [ zsh ];
     file = let
       files = [ ./configs/zsh/kubectl.zsh ./configs/zsh/prompt.zsh ];
-      src = builtins.filterSource (p: t: builtins.elem p files) ./.;
+      #src = builtins.filterSource (p: t: builtins.elem p files) ./.;
+      src = builtins.filterSource (p: t: builtins.elem p files);
     in { "${config.xdg.configHome}/.zshrc".text = builtins.readFile src; };
   };
 }
