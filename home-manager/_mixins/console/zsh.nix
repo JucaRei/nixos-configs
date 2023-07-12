752a3d5d736ccadbd2fe4a1b967b47ff768c68d2
Reinaldo <reinaldo800@gmail.com>
2023-07-12 14:29:06 -0300

    fix typo

--- home-manager/_mixins/console/zsh.nix
+++ home-manager/_mixins/console/zsh.nix
@@ -7,7 +7,7 @@
     packages = with pkgs; [ zsh ];
     file = {
       "${config.xdg.configHome}/.zshrc".text =
-        builtins.readFile .configs/zsh/kubectl.zsh + ./configs/zsh/prompt.zsh;
+        builtins.readFile ./configs/zsh/kubectl.zsh + ./configs/zsh/prompt.zsh;
     };
   };
 }
