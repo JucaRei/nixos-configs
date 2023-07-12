a86333a91dc139495a088da1909e579a265e3d81
Reinaldo <reinaldo800@gmail.com>
2023-07-12 15:16:15 -0300

    testing

--- home-manager/_mixins/console/zsh.nix
+++ home-manager/_mixins/console/zsh.nix
@@ -12,7 +12,7 @@
         "/configs/zsh/git.zsh"
       ];
       #src = builtins.filterSource (p: t: builtins.elem p files) ./.;
-      src = builtins.filterSource (p: t: builtins.elem p files) /.;
+      src = builtins.filterSource (p: t: builtins.elem p files) ././.;
     in { "${config.xdg.configHome}/.zshrc".text = builtins.readFile src; };
   };
 }
