c1c74f926fa9c573acde8ec57ec5dc5db60d7aaa
Reinaldo <reinaldo800@gmail.com>
2023-07-13 17:50:55 -0300

    fix htop

--- home-manager/_mixins/console/htop.nix
+++ home-manager/_mixins/console/htop.nix
@@ -8,7 +8,7 @@
       htop
     ];
     file = {
-      "${config.xdg.configHome}/.configs/htop/htoprc".text = builtins.readFile ./configs/htop/htoprc;
+      "${config.xdg.configHome}/.config/htop/htoprc".text = builtins.readFile ./configs/htop/htoprc;
     };
   };
 }
