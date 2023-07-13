87efd0b9ea070adf6910bafb4560e8f8f9062129
Reinaldo <reinaldo800@gmail.com>
2023-07-13 13:06:53 -0300

    need more debug

--- nixos/default.nix
+++ nixos/default.nix
@@ -437,10 +437,10 @@ in {
     ];
 
     # Reduce default service stop timeouts for faster shutdown
-    extraConfig = ''
-      DefaultTimeoutStopSec=15s
-      #DefaultTimeoutAbortSec=5s
-    '';
+    #extraConfig = ''
+    #  DefaultTimeoutStopSec=15s
+    #  DefaultTimeoutAbortSec=5s
+    #'';
   };
   system = {
 
