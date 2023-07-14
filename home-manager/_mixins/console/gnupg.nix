91ece933f2d8b48fec5578477c51e8775225898c
Reinaldo <reinaldo800@gmail.com>
2023-07-14 00:00:04 -0300

    fix error

--- home-manager/_mixins/console/gnupg.nix
+++ home-manager/_mixins/console/gnupg.nix
@@ -11,27 +11,27 @@
       #auto-key-import = true;
       #keyserver-options = "honor-keyserver-url";
     };
-    agent = {
-      enable = false;
-      # allows web browsers to access the gpg-agent daemon
-      #enableBrowserSocket = false;
-      # NOTE: "gnome3" flavor only works with Xorg
-      # To reload config: gpg-connect-agent reloadagent /bye
-      #pinentryFlavor = "gtk2"; # use "tty" for console only
-      pinentryFlavor = "cursers";
-      # cache SSH keys added by the ssh-add
-      enableSSHSupport = false;
-      # set up a Unix domain socket forwarding from a remote system
-      # enables to use gpg on the remote system without exposing the private keys to the remote system
-      enableExtraSocket = false;
-      enableBrowserSocket = false;
-      defaultCacheTtl = 34560000;
-      defaultCacheTtlSsh = 34560000;
-      #maxCacheTtl = 34560000;
-      #maxCacheTtlSsh = 34560000;
-      maxCacheTtl = 100*y; # effectively unlimited
-      maxCacheTtlSsh = 100*y; # effectively unlimited
-    };
+   # agent = {
+   #   enable = true;
+   #   # allows web browsers to access the gpg-agent daemon
+   #   #enableBrowserSocket = false;
+   #   # NOTE: "gnome3" flavor only works with Xorg
+   #   # To reload config: gpg-connect-agent reloadagent /bye
+   #   #pinentryFlavor = "gtk2"; # use "tty" for console only
+   #   #pinentryFlavor = "cursers";
+   #   # cache SSH keys added by the ssh-add
+   #   #enableSSHSupport = false;
+   #   # set up a Unix domain socket forwarding from a remote system
+   #   # enables to use gpg on the remote system without exposing the private keys to the remote system
+   #   enableExtraSocket = false;
+   #   enableBrowserSocket = false;
+   #   defaultCacheTtl = 34560000;
+   #   defaultCacheTtlSsh = 34560000;
+   #   #maxCacheTtl = 34560000;
+   #   #maxCacheTtlSsh = 34560000;
+   #   maxCacheTtl = 100*y; # effectively unlimited
+   #   maxCacheTtlSsh = 100*y; # effectively unlimited
+   # };
   };
 
    services.gpg-agent = {
