90d93585ddd6c2681049aa94cc94fa885e749e61
Reinaldo <reinaldo800@gmail.com>
2023-07-17 22:04:42 -0300

    fix

--- shell.nix
+++ shell.nix
@@ -36,6 +36,8 @@
       | |      | | | (_| | |   <  |  __/ \\__ \\
       |_|      |_|  \__,_| |_|\_\  \___| |___/
           "
+          "
+            export PS1="[\e[0;34m(Flakes)\$\e[m:\w]\$ "
       PATH=${
         pkgs.writeShellScriptBin "nix" ''
           ${pkgs.nixFlakes}/bin/nix --experimental-features "nix-command flakes repl-flake" "$@"
