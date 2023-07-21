8ad4726cc86643587c1157bab187d3d476fcd8ed
Reinaldo <reinaldo800@gmail.com>
2023-07-20 22:43:16 -0300

    fix

---
+++ nixos/_mixins/hardware/grub-bios.nix
@@ -0,0 +1,20 @@
+{ lib, ... }: {
+  boot.loader = {
+    efi = { canTouchEfiVariables = false; };
+    grub = {
+      enable = true;
+      efiSupport = false;
+      device = lib.mkDefault "/dev/sda"; # MBR/BIOS
+
+      backgroundColor = "#21202D";
+      configurationLimit = 6;
+
+      extraConfig = ''
+        set menu_color_normal=light-blue/black
+        set menu_color_highlight=black/light-blue
+      '';
+      splashMode = lib.mkDefault "normal";
+      splashImage = lib.mkDefault null;
+    };
+  };
+}
---
+++ nixos/_mixins/hardware/multiboot.nix
@@ -0,0 +1,5 @@
+# Dual boot with win10 https://github.com/ksevelyar/carbicide
+{ pkgs, ... }: {
+  boot.loader = { grub.useOSProber = true; };
+  environment.systemPackages = with pkgs; [ os-prober ];
+}
